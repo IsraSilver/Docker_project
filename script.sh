642eb8f5357e2da40c1e48c63ab3c73601283bb4
IsraSilver <109736225+IsraSilver@users.noreply.github.com>
2022-09-11 16:18:59 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! "$(docker ps | grep alpcon)" ]; then
     # run container
-        docker run -v /home/work:/home/public -d alpcon
+        docker run -v /home/work:/home/public --name must-die -d alpcon
 else echo "Container alredy running"
 fi
