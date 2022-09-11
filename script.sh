6b5a2248a3dc3b210b5c752bcdfed1b7d397aa4f
IsraSilver <109736225+IsraSilver@users.noreply.github.com>
2022-09-11 13:55:05 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! "$(docker ps | grep alpcon:0.0.1)" ]; then
     # run container
-        docker run -it alpcon:0.0.1
+        docker run -d alpcon:0.0.1
 else echo "Container alredy running"
 fi
